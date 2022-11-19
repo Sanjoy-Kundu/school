45d3d9a3be0691d96e9909a9a03accf22f5c0a62
sanjoy <sanjoykundu187@gamil.com>
2022-11-19 16:24:10 +0600

    nav bar added

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/*======================= HEADER JAVASCRIPT NAVBAR START ===============================*/
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
+/*======================= HEADER JAVASCRIPT NAVBAR END ===============================*/
