de24340e26493179cd6cce6e4cc1e59e6f26d78b
sanjoy <sanjoykundu187@gamil.com>
2022-11-19 17:17:07 +0600

    banner added

--- index.php
+++ index.php
@@ -24,10 +24,44 @@
     </div>
   </div>
   <a href="#about">About</a>
+  <a href="#registration" class="split">Registration</a>
+  <a href="#admin" class="split">admin</a>
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
 </div>
+
+<!-- <div class="slider">
+	<div class="container">
+		<div class="slider-item slider-active"><img src="images/image1.jpg" alt=""></div>
+		<div class="slider-item "><img src="images/image2.jpg" alt=""></div>
+		<div class="slider-item"><img src="images/image3.jpg" alt=""></div>
+	</div>
+</div> -->
+
+<div class="slidercontainer">  
+        <div class="showSlide fade">  
+            <img src="images/image1.jpg" />  
+            <div class="content">School website</div>  
+        </div>  
+        <div class="showSlide fade">  
+            <img src="images/image2.jpg"/>  
+            <div class="content">Lorem ipsum dolor sit amet</div>  
+        </div>  
+  
+        <div class="showSlide fade">  
+            <img src="images/image3.jpg"/>  
+            <div class="content">Lorem ipsum dolor sit amet</div>  
+        </div>  
+        <div class="showSlide fade">  
+            <img src="images/image1.jpg"/>  
+            <div class="content">Lorem ipsum dolor sit amet</div>  
+        </div>  
+        <!-- Navigation arrows -->  
+        <a class="left" onclick="nextSlide(-1)">❮</a>  
+        <a class="right" onclick="nextSlide(1)">❯</a>  
+    </div> 
 	</header>
 
+	
 
 
 
--- js/app.js
+++ js/app.js
@@ -9,3 +9,44 @@ function myFunction() {
 	}
 }
 /*======================= HEADER JAVASCRIPT NAVBAR END ===============================*/
+
+
+/*====================slider design start================== */
+/* setInterval(function () {
+	if (document.getElementsByClassName("slider-item")[0].classList.contains('slider-active')) {
+		document.getElementsByClassName('slider-item')[1].classList.add('slider-active');
+		document.getElementsByClassName('slider-item')[0].classList.remove('slider-active');
+	}
+	else if (document.getElementsByClassName('slider-item')[1].classList.contains('slider-active')) {
+		document.getElementsByClassName('slider-active')[2].classList.add('slider-active');
+		document.getElementsByClassName('slider-active')[1].classList.remove('slider-active');
+	}
+	else if (document.getElementsByClassName('slider-active')[2].classList.contains('slider-active')) {
+		document.getElementsByClassName('slider-active')[2].classList.add('slider-active');
+		document.getElementsByClassName('slider-active')[0].classList.remove('slider-active');
+	}
+}, 3000)
+ */
+
+var slide_index = 1;
+displaySlides(slide_index);
+
+function nextSlide(n) {
+	displaySlides(slide_index += n);
+}
+
+function currentSlide(n) {
+	displaySlides(slide_index = n);
+}
+
+function displaySlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("showSlide");
+	if (n > slides.length) { slide_index = 1 }
+	if (n < 1) { slide_index = slides.length }
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slides[slide_index - 1].style.display = "block";
+}
+/*====================slider design end================== */
